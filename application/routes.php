ca1e63ccfc280cee5a528c106c5201c149117727
Roman Zhukov <roman@jadeite.su>
2021-12-08 19:48:15 +0500

    WIP: almost done

--- application/routes.php
+++ application/routes.php
@@ -3,4 +3,7 @@ use Pecee\SimpleRouter\SimpleRouter;
 SimpleRouter::get('/', function() {
     return phpinfo();
 });
+SimpleRouter::get('/health/check', function() {
+    return "I'm running on " . getenv('KUBE_NODE_NAME') . " - " . getenv('KUBE_NODE_IP');
+});
 ?>
